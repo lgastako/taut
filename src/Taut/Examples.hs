b64bc8896e5af828b232286271c2d3d01fb2bc45
John Evans <lgastako@gmail.com>
2017-06-25 00:43:37 -0600

    Improve lens readablity in example 1.

--- src/Taut/Examples.hs
+++ src/Taut/Examples.hs
@@ -2,11 +2,13 @@ module Taut.Examples
        ( exampleMsg1
        ) where
 
-import           Control.Lens                            ( (.~) )
+import           Control.Lens                            ( (&)
+                                                         , (.~)
+                                                         )
 import           Data.Text                               ( Text )
 import           Taut
 import           Taut.Types.MessageEvent                 ( payload )
 import qualified Taut.Types.MessageEvent as MessageEvent
 
 exampleMsg1 :: MessageEvent Text
-exampleMsg1 = payload .~ "Example message #1." $ MessageEvent.empty
+exampleMsg1 = MessageEvent.empty & payload .~ "Example message #1."
