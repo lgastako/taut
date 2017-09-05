f2eb70b8c8eee4cc2a01ad1b3c7426c2f3bb96ff
mpdairy <mpdairy@gmail.com>
2017-09-05 14:21:54 -0600

    teamid

---
+++ src/Taut/Types/TeamId.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Taut.Types.TeamId
+       ( TeamId
+       , fromText
+       , toText
+       , tidText
+       ) where
+
+import Control.Lens              ( Iso'
+                                 , iso
+                                 )
+import Data.Aeson.TH             ( defaultOptions
+                                 , deriveJSON
+                                 )
+import Data.Csv                  ( ToField
+                                 , toField
+                                 )
+import Data.DeriveTH             ( derive
+                                 , makeArbitrary
+                                 )
+import Data.Text.Encoding        ( encodeUtf8 )
+import Infinity
+import Test.QuickCheck           ( Arbitrary
+                                 , arbitrary
+                                 )
+import Test.QuickCheck.Instances ()
+
+newtype TeamId = TeamId Text
+  deriving (Eq, Ord, Read, Show)
+
+instance ToField TeamId where
+  toField = encodeUtf8 . toText
+
+fromText :: Text -> TeamId
+fromText = TeamId
+
+toText :: TeamId -> Text
+toText (TeamId u) = u
+
+tidText :: Iso' TeamId Text
+tidText = iso toText fromText
+
+$(deriveJSON defaultOptions ''TeamId)
+
+derive makeArbitrary ''TeamId
