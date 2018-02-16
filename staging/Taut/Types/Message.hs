45004ef085e52f416ca4a8e43ccca19f80bab5ff
mpdairy <mpdairy@gmail.com>
2018-02-15 18:46:53 -0700

    chaning

--- staging/Taut/Types/Message.hs
+++ staging/Taut/Types/Message.hs
@@ -1,50 +1,54 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE TemplateHaskell #-}
-module Taut.Types.Message
-       ( Parse( Full, None )
-       , Message( Message )
-       , token
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell   #-}
+module Slack.Types.Message
+       ( Message( Message )
+       , Parse( Full
+              , None
+              )
+       , asUser
+       , attachments
        , botId
        , channel
-       , text
-       , parse
+       , iconUrl
+       , iconEmoji
        , linkNames
-       , attachments
+       , parse
+       , replyBroadcast
+       , subType
+       , text
+       , threadTs
+       , token
+       , ts
+       , type'
        , unfurlLinks
        , unfurlMedia
        , username
-       , asUser
-       , iconUrl
-       , iconEmoji
-       , threadTs
-       , replyBroadcast
-       , type'
-       , subType
-       , ts
-       , empty
        )
        where
 
-import Control.Lens                  ( makeLenses )
-import Data.Aeson                    ( FromJSON( parseJSON )
-                                     , ToJSON( toJSON )
-                                     , defaultOptions
-                                     , genericParseJSON
-                                     , genericToJSON
-                                     )
-import Data.Aeson.Types              ( Options( constructorTagModifier
-                                              , fieldLabelModifier
-                                              , omitNothingFields
-                                              )
-                                     , camelTo2
-                                     )
-import Data.Char                     ( toLower )
-import Data.Text                     ( Text )
-import GHC.Generics                  ( Generic )
-import Taut.Types.ChannelId          ( ChannelId )
+import Control.Lens                   ( makeLenses )
+import Data.Aeson                     ( FromJSON( parseJSON )
+                                      , ToJSON( toJSON )
+                                      , defaultOptions
+                                      , genericParseJSON
+                                      , genericToJSON
+                                      )
+import Data.Aeson.Types               ( Options( constructorTagModifier
+                                               , fieldLabelModifier
+                                               , omitNothingFields
+                                               )
+                                      , camelTo2
+                                      )
+import Data.Char                      ( toLower )
+import Data.Default                   ( Default
+                                      , def
+                                      )
+import Focus.Prelude
 import Taut.Types.Message.Attachment ( Attachment )
-import Taut.Types.OauthToken         ( OauthToken )
-import Taut.Types.UserName           ( UserName )
+import Taut.Types.ChannelId           ( ChannelId )
+import Taut.Types.OauthToken          ( OauthToken )
+import Taut.Types.UserName            ( UserName )
 
 data Parse = Full | None
   deriving (Generic, Show)
@@ -61,26 +65,25 @@ customUnionTypeOptions = defaultOptions
   }
 
 data Message a = Message
-  { _token :: Maybe OauthToken
-  , _botId :: Maybe Text
-  , _channel :: Maybe ChannelId
-  , _text :: Maybe Text
-  , _parse :: Maybe Parse
-  , _linkNames :: Maybe Bool
-  , _attachments :: Maybe [Attachment a]
-  , _unfurlLinks :: Maybe Bool
-  , _unfurlMedia :: Maybe Bool
-  , _username :: Maybe UserName
-  , _asUser :: Maybe Bool
-  , _iconUrl :: Maybe Text
-  , _iconEmoji :: Maybe Text
-  , _threadTs :: Maybe Text
+  { _token          :: Maybe OauthToken
+  , _botId          :: Maybe Text
+  , _channel        :: Maybe ChannelId
+  , _text           :: Maybe Text
+  , _parse          :: Maybe Parse
+  , _linkNames      :: Maybe Bool
+  , _attachments    :: Maybe [Attachment a]
+  , _unfurlLinks    :: Maybe Bool
+  , _unfurlMedia    :: Maybe Bool
+  , _username       :: Maybe UserName
+  , _asUser         :: Maybe Bool
+  , _iconUrl        :: Maybe Text
+  , _iconEmoji      :: Maybe Text
+  , _threadTs       :: Maybe Text
   , _replyBroadcast :: Maybe Bool
-  , _type' :: Maybe Text
-  , _subType :: Maybe Text
-  , _ts :: Maybe Text
-  }
-  deriving (Show, Generic)
+  , _type'          :: Maybe Text
+  , _subType        :: Maybe Text
+  , _ts             :: Maybe Text
+  } deriving (Show, Generic)
 
 makeLenses ''Message
 
@@ -96,24 +99,25 @@ customOptions = defaultOptions
   , omitNothingFields = True
   }
 
-empty :: Message a
-empty = Message
-  { _token = Nothing
-  , _botId = Nothing
-  , _channel = Nothing
-  , _text = Nothing
-  , _parse = Nothing
-  , _linkNames = Nothing
-  , _attachments = Nothing
-  , _unfurlLinks = Nothing
-  , _unfurlMedia = Nothing
-  , _username = Nothing
-  , _asUser = Nothing
-  , _iconUrl = Nothing
-  , _iconEmoji = Nothing
-  , _threadTs = Nothing
-  , _replyBroadcast = Nothing
-  , _type' = Nothing
-  , _subType = Nothing
-  , _ts = Nothing
-  }
+instance Default (Message a) where
+  def = Message
+    { _asUser         = Nothing
+    , _attachments    = Nothing
+    , _botId          = Nothing
+    , _channel        = Nothing
+    , _iconEmoji      = Nothing
+    , _iconUrl        = Nothing
+    , _linkNames      = Nothing
+    , _parse          = Nothing
+    , _text           = Nothing
+    , _token          = Nothing
+    , _unfurlLinks    = Nothing
+    , _unfurlMedia    = Nothing
+    , _username       = Nothing
+    , _replyBroadcast = Nothing
+    , _subType        = Nothing
+    , _threadTs       = Nothing
+    , _ts             = Nothing
+    , _type'          = Nothing
+    }
+
