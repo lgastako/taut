07a8f4512f1a2e6513770fc27d4b5c3cb004eaab
John Evans <lgastako@gmail.com>
2018-03-06 19:10:29 -0700

    Provide generic To/FromJSON for BotEvent.

--- src/Taut/Types/BotEvent.hs
+++ src/Taut/Types/BotEvent.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Taut.Types.BotEvent
      ( BotEvent(..)
      ) where
 
-import Data.Aeson           ( Value )
+import Data.Aeson           ( FromJSON
+                            , ToJSON
+                            , Value
+                            )
 import Data.Text            ( Text )
+import GHC.Generics         ( Generic )
 import Taut.Types.ChannelId ( ChannelId )
 import Taut.Types.EditInfo  ( EditInfo )
 import Taut.Types.Timestamp ( Timestamp )
@@ -13,6 +19,9 @@ data BotEvent
   = Message ChannelId UserId Text Timestamp (Maybe Subtype) (Maybe EditInfo)
   | OtherEvent
   | UnknownEvent Value
-  deriving (Eq, Read, Show)
+  deriving (Eq, Generic, Read, Show)
 
 type Subtype = Text
+
+instance FromJSON BotEvent
+instance ToJSON   BotEvent
